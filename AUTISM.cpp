f0d737a9599320a678603a659ed0882df94141f1
LostC0de <72858400+LostC0de@users.noreply.github.com>
2022-09-09 00:05:34 +0500

    Create AUTISM.cpp

---
+++ AUTISM.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	if ((N < 0) or (N > 100)) {
+		cout << "Ti Daun?";
+	}
+	
+	if (N == 1 or N == 21 or N == 31 or N == 41 or N == 51 or N == 61 or N == 71 or N == 81 or N == 91)
+	{
+		cout << N << " God";
+		
+
+		
+	}
+	if (N == 2 or N == 3 or N == 4 or N == 22 or N == 23 or N == 24 or N == 32 or N == 33 or N == 34 or N == 42 or N == 43 or N == 43 or N == 44 or N == 52 or N == 53 or N == 54 or N == 62 or N == 63 or N == 64 or N == 72 or N == 73 or N == 74 or N == 82 or N == 83 or N == 84 or N == 92 or N == 93 or N == 94)
+	{
+		cout << N << " Goda";
+	}
+	if (N == 5 or N == 6 or N == 7 or N == 8 or N == 9 or N == 10 or N == 11 or N == 12 or N == 13 or N == 14 or N == 15 or N == 16 or N == 17 or N == 18 or N == 19 or N == 20 or N == 25 or N == 26 or N == 27 or N == 28 or N == 29 or N == 30 or N == 35 or N == 36 or N == 37 or N == 38 or N == 39 or N == 40 or N == 45 or N == 46 or N == 47 or N == 48 or N == 49 or N == 50 or N == 55 or N == 56 or N == 57 or N == 58 or N == 59 or N == 60 or N == 65 or N == 66 or N == 67 or N == 68 or N == 69 or N == 70 or N == 75 or N == 76 or N == 77 or N == 78 or N == 79 or N == 80 or N == 85 or N == 86 or N == 87 or N == 88 or N == 89 or N == 90 or N == 95 or N == 96 or N == 97 or N == 98 or N == 99 or N == 100)
+	{
+		cout << N << " Let";
+	}
+}
+
+
